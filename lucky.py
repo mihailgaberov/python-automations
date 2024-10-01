2c1cecc7f5b9e90e624a245eba1cdb6970cd8d70
Mihail Gaberov <mihail.gaberov@gmail.com>
2024-10-01 13:07:15 +0200

    chore: testing

--- lucky.py
+++ lucky.py
@@ -3,15 +3,18 @@
 
 import requests, sys, webbrowser, bs4
 
-print('Googling...') # display text while downloading the Google page
-res = requests.get('https://google.com/search?q=' + ' '.join(sys.argv[1:]))
+print('Googling...')
+# res = requests.get('https://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res = requests.get('https://duckduckgo.com/?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
 # Retrieve top search result links.
 soup = bs4.BeautifulSoup(res.text, features="html.parser")
 
 # Open a browser tab for each result.
-linkElems = soup.select('.kCrYT a')
+linkElems = soup.select('.wLL07_0Xnd1QZpzpfR4W a')
+print(linkElems)
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
-    webbrowser.open('https://google.com' + linkElems[i].get('href'))
+    # webbrowser.open('https://google.com' + linkElems[i].get('href'))
+    webbrowser.open('https://duckduckgo.com' + linkElems[i].get('href'))
